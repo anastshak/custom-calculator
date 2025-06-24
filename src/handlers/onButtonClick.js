d2432da21a26001b813f3a18627e00315fcebee0
anastshak <nastyashakura@gmail.com>
2025-06-24 16:32:20 +0300

    feat: implement memory handler

--- src/handlers/onButtonClick.js
+++ src/handlers/onButtonClick.js
@@ -1,13 +1,16 @@
-import { operatorValues, functionValues } from '../constants/constants';
+import { operatorValues, functionValues, memoryValues } from '../constants/constants';
 import handleOperator from './handleOperator';
 import handleFunction from './handleFunction';
 import handleNumber from './handleNumber';
+import handleMemory from './handleMemory';
 
 export default function onButtonClick(value, display, extraDisplay) {
   if (operatorValues.includes(value)) {
     handleOperator(value, display, extraDisplay);
   } else if (functionValues.includes(value)) {
     handleFunction(value, display, extraDisplay);
+  } else if (memoryValues.includes(value)) {
+    handleMemory(value, display);
   } else {
     handleNumber(value, display);
   }
