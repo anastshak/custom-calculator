f14f158ee3aa5b80b58af036494718e8eb3b92b5
anastshak <nastyashakura@gmail.com>
2025-06-26 12:05:13 +0300

    feat: create a separate command for AC

---
+++ src/commands/ClearCommand.js
@@ -0,0 +1,27 @@
+import Command from './Command';
+import { getState, updateState, clearCalculation } from '../state/state';
+
+export default class ClearCommand extends Command {
+  constructor(display, extraDisplay) {
+    super();
+    this.display = display;
+    this.extraDisplay = extraDisplay;
+
+    // сохраняем состояние до очистки
+    this.prevState = getState();
+    this.prevDisplay = display.value;
+    this.prevExtraDisplay = extraDisplay.textContent;
+  }
+
+  execute() {
+    clearCalculation();
+    this.display.value = '';
+    this.extraDisplay.textContent = '';
+  }
+
+  undo() {
+    updateState(this.prevState);
+    this.display.value = this.prevDisplay;
+    this.extraDisplay.textContent = this.prevExtraDisplay;
+  }
+}
