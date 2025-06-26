c575908688943876ddf3627309caa75bae97740b
anastshak <nastyashakura@gmail.com>
2025-06-26 12:10:12 +0300

    feat: rewrite common button click handler

--- src/handlers/onButtonClick.js
+++ src/handlers/onButtonClick.js
@@ -1,17 +1,47 @@
 import { operatorValues, functionValues, memoryValues } from '../constants/constants';
-import handleOperator from './handleOperator';
-import handleFunction from './handleFunction';
-import handleNumber from './handleNumber';
-import handleMemory from './handleMemory';
+// import handleOperator from './handleOperator';
+// import handleFunction from './handleFunction';
+// import handleNumber from './handleNumber';
+// import handleMemory from './handleMemory';
+
+import { commandManager } from '../commands/CommandManager';
+import NumberCommand from '../commands/NumberCommand';
+import OperatorCommand from '../commands/OperatorCommand';
+import ClearCommand from '../commands/ClearCommand';
+import MemoryCommand from '../commands/MemoryCommand';
+
+// import { isValidNumber } from '../utils/utils';
+import FunctionCommand from '../commands/FunctionCommand';
 
 export default function onButtonClick(value, display, extraDisplay) {
+  // undo
+  if (value === '←') {
+    commandManager.undo();
+    return;
+  }
+
   if (operatorValues.includes(value)) {
-    handleOperator(value, display, extraDisplay);
+    // + - * /
+    const command = new OperatorCommand(value, display, extraDisplay);
+    commandManager.executeCommand(command);
   } else if (functionValues.includes(value)) {
-    handleFunction(value, display, extraDisplay);
+    // functions
+    if (value === 'AC') {
+      // AC
+      const command = new ClearCommand(display, extraDisplay);
+      commandManager.executeCommand(command);
+      return;
+    } else {
+      const command = new FunctionCommand({ display, extraDisplay, funcName: value });
+      commandManager.executeCommand(command);
+    }
   } else if (memoryValues.includes(value)) {
-    handleMemory(value, display);
+    // MC M+ M- MR
+    const command = new MemoryCommand(value, display);
+    commandManager.executeCommand(command);
   } else {
-    handleNumber(value, display);
+    // numbers
+    const command = new NumberCommand(value, display);
+    commandManager.executeCommand(command);
   }
 }
