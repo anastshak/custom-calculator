44df82396c9a9f932f6cf819f333ce78280f735c
anastshak <nastyashakura@gmail.com>
2025-06-26 12:09:08 +0300

    feat: convert number handler

---
+++ src/commands/NumberCommand.js
@@ -0,0 +1,35 @@
+// input numbers
+import Command from './Command';
+import { getState, updateState } from '../state/state';
+
+export default class NumberCommand extends Command {
+  constructor(value, display) {
+    super();
+    this.value = value;
+    this.display = display;
+
+    const { currentOperand } = getState();
+    this.prevValue = currentOperand;
+  }
+
+  execute() {
+    const { shouldClearDisplay } = getState();
+
+    let newValue;
+
+    if (shouldClearDisplay) {
+      newValue = this.value === '.' ? '0.' : this.value;
+      updateState({ shouldClearDisplay: false }); // сброс флага
+    } else {
+      newValue = this.value === '.' && this.prevValue === '' ? '0.' : this.prevValue + this.value;
+    }
+
+    this.display.value = newValue;
+    updateState({ currentOperand: newValue });
+  }
+
+  undo() {
+    this.display.value = this.prevValue;
+    updateState({ currentOperand: this.prevValue, shouldClearDisplay: false });
+  }
+}
